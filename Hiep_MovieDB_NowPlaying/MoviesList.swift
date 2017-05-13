460ddb6433256f94b8952333ed52565ef92e94ef
cntt17 <cntt17@cntt17s-iMac.local>
2017-05-13 17:11:37 +0700

    Movies 20

---
+++ Hiep_MovieDB_NowPlaying/MoviesList.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesList.swift
+//  Hiep_MovieDB_NowPlaying
+//
+//  Created by cntt17 on 5/13/17.
+//  Copyright © 2017 cntt17. All rights reserved.
+//
+
+import Foundation
+
+class MovieList {
+    
+    init() {
+        
+    }
+    
+    public class func movies() -> [Movie] {
+        let movieList: Array<Movie> = []
+        return movieList
+    }
+    
+    
+}
